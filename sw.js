c7daa62287f3b408913ba064c4251d2948a48e87
Ngo Cristal <cristal@MacBook-Air-2.local>
2026-09-14 18:05:53 +0700

    Keep Gallery controls below slide content

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "gnosis-hanoi-shell-v14";
+const SHELL_CACHE = "gnosis-hanoi-shell-v15";
 const AUDIO_CACHE = "gnosis-hanoi-offline-audio-v1";
 const CATALOG_PATH = new URL("./data/catalog.json", self.registration.scope).pathname;
 const SHELL_ASSETS = [
